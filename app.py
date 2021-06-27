89c86460c0a0a9bb20bcf947ed3d3e1d87b685f9
lukew3 <lukew25073@gmail.com>
2021-06-26 23:04:04 -0400

    Added recolor routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, jsonify, Response
+import requests, uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aslkfjhcaniewjnaefs'
@@ -8,6 +9,20 @@ app.config['SECRET_KEY'] = 'aslkfjhcaniewjnaefs'
 def home():
    return render_template("home.html" )
 
+@app.route('/recolorbot/upload', methods=['POST'])
+def recolorBotUpload():
+    url = request.args['url']
+    r = requests.get(url)
+    
+    filename = str(uuid.uuid4()) + ".jpg"
+    with open(f'recolorbot/photos/{filename}', 'wb') as f:
+        f.write(r.content)
+
+    return jsonify(url=f"https://lukew3.com/recolorbot/photos/{filename}")
+
+@app.route('/recolorbot/photos/<filename>', methods=['GET'])
+def recolorDisplayImage(filename):
+    return send_from_directory('./recolorbot/photos', filename)    
 
 if __name__ == "__main__":
     app.run()
