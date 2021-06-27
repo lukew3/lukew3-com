b86586b6d4764ecf9ceae628fa32b848177d514a
lukew3 <lukew25073@gmail.com>
2021-06-27 14:07:52 -0400

    Removed recolorbot upload route

--- app.py
+++ app.py
@@ -9,17 +9,6 @@ app.config['SECRET_KEY'] = 'aslkfjhcaniewjnaefs'
 def home():
    return render_template("home.html" )
 
-@app.route('/recolorbot/upload', methods=['POST'])
-def recolorBotUpload():
-    url = request.args['url']
-    r = requests.get(url)
-    
-    filename = str(uuid.uuid4()) + ".jpg"
-    with open(f'recolorbot/photos/{filename}', 'wb') as f:
-        f.write(r.content)
-
-    return jsonify(url=f"https://lukew3.com/recolorbot/photos/{filename}")
-
 @app.route('/recolorbot/photos/<filename>', methods=['GET'])
 def recolorDisplayImage(filename):
     return send_from_directory('./recolorbot/photos', filename)    
