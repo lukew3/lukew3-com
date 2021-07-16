d304a34d1ca42ed2cae6fc40c3b6e58e034a6bb6
lukew3 <lukew25073@gmail.com>
2021-07-16 00:01:32 -0400

    Added journal

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, jsonify, Response
 import requests, uuid
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aslkfjhcaniewjnaefs'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/luke/journal.db'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -25,5 +29,35 @@ def recolorDisplayImage(filename):
 def recolorBotReroute():
     return redirect("https://www.reddit.com/user/recolorbot")
 
+db = SQLAlchemy(app)
+class JournalEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(), nullable=False)
+    year = db.Column(db.Integer)
+    month = db.Column(db.Integer)
+    day = db.Column(db.Integer)
+    # Time should always be in 24 hour format eastern time
+    time = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'Entry {self.month}/{self.day}/{self.year} {self.time} - {self.text}'
+
+@app.route('/jrnl', methods=['GET', 'POST'])
+def add_journal_entry():
+    if request.method == 'POST':
+        text = request.form.get('entry')
+        if text == "":
+            return render_template("journal.html")
+        now = datetime.now(pytz.timezone('America/New_York'))
+        new_entry = JournalEntry(text=text, 
+                year=now.strftime("%Y"),
+                month=now.strftime("%m"),
+                day=now.strftime("%d"),
+                time=now.strftime("%H:%M:%S"))
+        db.session.add(new_entry)
+        db.session.commit()
+    return render_template("journal.html")
+
 if __name__ == "__main__":
     app.run()
+
